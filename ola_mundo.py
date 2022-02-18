5a1ac0a59d5be5b657b08aa6be2a0ece9373412c
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-17 23:33:13 -0300

    Primeiro codito

---
+++ ola_mundo.py
@@ -0,0 +1,21 @@
+#Meu primeiro codigo
+"""Essa forma e para
+criar comentarios maiores"""
+'''primeiro comentario
+    segundo comentario
+    terceiro comentario
+    quarto comentario
+    quinto comenatio'''
+
+"""
+    Nome:Tassio Sales
+    data: 17/02/2020
+"""
+
+print("ola Mundo")
+
+print("O que temos para hoje")
+
+print("Como e otimo estudar python")
+
+print("Texto são escritos entre aspasas")
