7c72eea04235d80b1767a553dd4b3d7adb0f053a
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-21 10:23:21 -0300

    add formated metodos

---
+++ metodos_string.py
@@ -0,0 +1,15 @@
+mensagem = "Eu adoro comida Caseira"
+mensagem2 = "                    Eu adoro comida Caseira"
+
+print(mensagem)
+
+print(mensagem.upper())
+print(mensagem.lower())
+print(mensagem.capitalize())
+print(mensagem.find('c'))
+print(mensagem.find('C'))
+print(mensagem.find("adoro"))
+print(mensagem.replace("adoro", "amo"))
+print(mensagem.replace("a", 'e'))
+print(mensagem.replace("Caseira", 'Feita em casa'))
+print(mensagem2.strip())
