614902cafd83d725a6c6ac326bf95b61e5cfad08
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-21 11:05:48 -0300

    add operacoes matematicas

---
+++ operadores_comparacao.py
@@ -0,0 +1,16 @@
+# == Equal
+# != Not equal
+# > Greater than
+# < Less than
+# >= Greater than or Equal to
+# <= Less than or Equal to
+print(10 == 10)
+print(10 == 11)
+print(10 > 13)
+print(10 > 9)
+print(10 <= 9)
+print(10 <= 10)
+print(10 >= 10)
+print("banana" == "banana")
+print("banana" == "BANANA")
+print("banana" != "BANANA")
