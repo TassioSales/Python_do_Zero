199c3ea688ee10ca3e4f6dba7eca090443abb55b
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-21 10:00:33 -0300

    add slice e calculadora

---
+++ slice.py
@@ -0,0 +1,18 @@
+fruta = 'Abacate'
+#. index 0123456
+print(fruta)
+
+print(fruta[3])
+
+print(fruta[2:6])
+
+print(fruta[-1])
+
+valor = 99.75
+valor = str(valor)
+
+print(valor[3:5])
+
+print(valor[:2])
+
+print(valor[2:])
