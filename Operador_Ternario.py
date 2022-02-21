5151ebe4002c0d4b8726c9d9674d10fd55646878
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-21 17:09:53 -0300

    add operadores ternarios

---
+++ Operador_Ternario.py
@@ -0,0 +1,9 @@
+idade = 17
+
+if idade >= 16:
+    print('Voto permitido')
+else:
+    print("Voto nao e permitido")
+
+resultado = "Voto Permitido" if idade >= 16 else 'Voto Negado'
+print(resultado)
