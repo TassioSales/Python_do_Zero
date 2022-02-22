f656c4d3dab205aba2890500c6d762b129b66eba
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-22 10:49:28 -0300

    como funciona uma função

---
+++ Como_funciona_uma_funcao.py
@@ -0,0 +1,6 @@
+def boasvindas():
+    print('Olá Tássio')
+    print("Temos 5 laptops")
+
+
+boasvindas()
---
+++ Criando_uma_funcao_de_soma.py
@@ -0,0 +1,25 @@
+num1 = 10
+num2 = 7
+
+def somar_dois_numeros_v0():
+    num1 = 10
+    num2 = 5
+    soma = num1 + num2
+    print(soma)
+
+
+def somar_dois_numeros_v1():
+    num1 = 5
+    num2 = 6
+    soma = num1 + num2
+    print(soma)
+
+
+def somar_dois_numeros_v2(num1, num2):
+    soma = num1 + num2
+    print(soma)
+
+
+somar_dois_numeros_v0()
+somar_dois_numeros_v1()
+somar_dois_numeros_v2(num1, num2)
