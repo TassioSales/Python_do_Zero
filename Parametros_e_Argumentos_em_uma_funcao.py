76d5f3801a04b021dee4b5845cc62cf3990b9f21
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-22 13:18:28 -0300

    Parametros e Argumentos em uma função

---
+++ Parametros_e_Argumentos_em_uma_funcao.py
@@ -0,0 +1,11 @@
+nome = input("digite seu nome: ")
+produto = input('Digite o nome do produto desejado: ')
+
+
+def boasvindas(nome, produto):
+    print(f'Bem vindo {nome}')
+    print(f'Temos 5 {produto}s em estoque ')
+
+
+boasvindas(nome, produto)
+
