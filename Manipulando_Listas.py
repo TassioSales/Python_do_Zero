fa77169c7be29ea61d9bcb52aab4d2fc37f5a883
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-24 22:21:14 -0300

    Manipulando Listas

---
+++ Manipulando_Listas.py
@@ -0,0 +1,17 @@
+numero = [i for i in range(0, 100, 2)]
+print(numero)
+
+cidade1 = "Rio de Janeiro"
+cidade2 = 'Brasilia'
+cidade3 = 'São Paulo'
+
+cidades = ["Rio de Janeiro", 'Brasilia', 'São Paulo']
+
+print(cidades)
+print(cidades[0])
+print(cidades[1])
+print(cidades[2])
+
+cidades[0] = 'Sergipe'
+
+print(cidades)
