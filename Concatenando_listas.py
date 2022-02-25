52a9848a66f77412e2c06ddb2f9ae249bc4f6982
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-24 22:55:38 -0300

    Concatenando listas

---
+++ Concatenando_listas.py
@@ -0,0 +1,19 @@
+numeros = [2, 3, 4, 5]
+letras = ['a', 'b', 'c', 'd']
+
+concatenacao = numeros + letras
+
+numeros.extend(letras)
+
+print(numeros)
+print(concatenacao)
+
+itens = ['item1', 'item2', 'item3', 'item4']
+
+itens1 = [itens[0], itens[1]]
+itens2 = [itens[2], itens[3]]
+
+print(itens)
+
+print(itens1)
+print(itens2)
