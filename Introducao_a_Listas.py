cdec52d0889bf9270969277a5daac4f1a1a5a4d0
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-24 22:13:28 -0300

    Introdução a Listas

---
+++ Introducao_a_Listas.py
@@ -0,0 +1,10 @@
+cidade1 = "Rio de Janeiro"
+cidade2 = 'Brasilia'
+cidade3 = 'São Paulo'
+
+cidades = ["Rio de Janeiro", 'Brasilia', 'São Paulo']
+
+print(cidades)
+print(cidades[0])
+print(cidades[1])
+print(cidades[2])
