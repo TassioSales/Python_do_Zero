ee7d620152792f8b0268f8eb71b5e536f9355e23
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-24 22:42:52 -0300

    Funções dentro de Listas

---
+++ Funcoes_dentro_de_Listas.py
@@ -0,0 +1,17 @@
+cidades = ["Rio de Janeiro", 'Brasilia', 'São Paulo']
+print(cidades)
+
+cidades.append('Maceio')
+print(cidades)
+
+cidades.remove('Maceio')
+print(cidades)
+
+cidades.insert(0, "Maceio")
+print(cidades)
+
+cidades.pop(0)
+print(cidades)
+
+cidades.sort()
+print(cidades)
