0ad5d2ae13e039701ea6881a63a9da93d187c1c1
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-02-26 20:18:45 -0300

    Sets com strings

---
+++ Sets_com_strings.py
@@ -0,0 +1,13 @@
+set1 = {'a', 'b', 'c', 'd'}
+set2 = {'a', 'd', 'e'}
+set3 = {'c', 'd', 'f'}
+
+set4 = set1.union(set2)
+set5 = set1.difference(set3)
+set6 = set1.intersection(set2)
+set7 = set1.symmetric_difference(set3)
+
+print(set4)
+print(set5)
+print(set6)
+print(set7)
