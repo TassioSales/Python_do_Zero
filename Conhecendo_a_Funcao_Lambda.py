2bdcaa81ab76ffbbaa404feea43e51f66e2629f7
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-03-02 22:56:08 -0300

    Conhecendo a Função Lambda

---
+++ Conhecendo_a_Funcao_Lambda.py
@@ -0,0 +1,15 @@
+def somar(x):
+    return x + 10
+
+
+resultado = somar(2)
+
+print(resultado)
+
+# lambda
+
+resultado_2 = lambda x: x + 10
+print(resultado_2(2))
+
+resultado_3 = lambda x, y: x + y + 10
+print(resultado_3(2, 4))
