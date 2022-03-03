f3b7be94d28699e6ea3aad152419f55540d5e0aa
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-03-02 22:45:42 -0300

    Visualizando Itens, Keys e Values

---
+++ Visualizando_Itens_Keys_e_Values.py
@@ -0,0 +1,12 @@
+aluno = {'nome': 'Ana',
+         'idade': 16,
+         'nota_final': 'A',
+         'aprovacao': True,
+         'Materias': ['Fisica', 'Matematica', 'Ingles']}
+
+print(aluno.get('Materias'))
+print(len(aluno))
+print(aluno.values())
+print(aluno.keys())
+print(aluno.items())
+
