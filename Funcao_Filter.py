265d2c4f98bd581906a3fa1f5ede8a40f5a05362
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-03-03 19:35:51 -0300

    Função Filter

---
+++ Funcao_Filter.py
@@ -0,0 +1,21 @@
+valores = [10, 12, 34, 44, 57]
+
+valores2 = [x * 2 for x in range(0, 50)]
+
+
+def remover20(x):
+    return x > 20
+
+
+var1 = list(map(remover20, valores2))
+var2 = list(map(remover20, valores))
+var3 = list(filter(remover20, valores2))
+var4 = list(filter(remover20, valores))
+var5 = list(filter(lambda x: x > 30, valores))
+var5 = list(filter(lambda x: x > 50, valores2))
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
