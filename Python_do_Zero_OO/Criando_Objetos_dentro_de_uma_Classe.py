dd19b4d30b444d372be52e181aeaf82ccc42e487
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-03-07 17:33:03 -0300

    Criando Construtores

---
+++ Python_do_Zero_OO/Criando_Objetos_dentro_de_uma_Classe.py
@@ -0,0 +1,19 @@
+# criar a classe
+class Funcionario:
+    pass
+
+
+# criando objeto
+usuario1 = Funcionario()
+usuario2 = Funcionario()
+
+usuario1.nome = "Tassio"
+usuario1.sobrenome = "Sales"
+usuario1.data_nascimento = "02/04/1990"
+
+usuario2.nome = "Lucian"
+usuario2.sobrenome = "Jesus"
+usuario2.data_nascimento = "25/05/2002"
+
+print(usuario1.nome)
+print(usuario2.nome)
