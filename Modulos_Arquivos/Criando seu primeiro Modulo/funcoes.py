db3e3848047769b6a7de16dfdfdbcce77a366f7a
TassioSales <74218122+TassioSales@users.noreply.github.com>
2022-03-09 00:31:25 -0300

    Criando seu primeiro Modulo

---
+++ Modulos_Arquivos/Criando seu primeiro Modulo/funcoes.py
@@ -0,0 +1,12 @@
+class Funcoes:
+    def __init__(self, valor1, valor2):
+        self.valor1 = valor1
+        self.valor2 = valor2
+
+    def somar(self):
+        soma =  self.valor1 + self.valor2
+        print(soma)
+
+    def multi(self):
+        multiplicar = self.valor1 * self.valor2
+        print(multiplicar)
---
+++ Modulos_Arquivos/Criando seu primeiro Modulo/main.py
@@ -0,0 +1,12 @@
+from funcoes import Funcoes
+
+var1 = Funcoes(5, 2)
+var2 = Funcoes(100, 22)
+
+var1.somar()
+var1.multi()
+
+var2.somar()
+var2.multi()
+
+
